7c03256169dc4729098fb7e5d07690e752cf736e
Maxim Britvin <maksbritvin@gmail.com>
2022-10-25 00:32:20 +0300

    tests update

--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-from unittest import TestCase
+from unittest import TestCase, main
 from main import simple_func
 
 class SimpeTest(TestCase):
@@ -8,3 +8,6 @@ class SimpeTest(TestCase):
         age = 30
         test_str = simple_func(name, age)
         self.assertEqual(test_str, "My name is %s. Age is %s" % (name, age))
+        
+if __name__ == "__main__":
+    main()
